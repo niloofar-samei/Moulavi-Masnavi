7ba77ecd8a602b69602692c92c48e858180df297
niloofar <niloofar.samei@gmail.com>
2018-05-16 04:23:37 +0430

    edit anh delete def start_requests

--- moulavi/moulavi/spiders/moulavi_spider.py
+++ moulavi/moulavi/spiders/moulavi_spider.py
@@ -1,17 +1,15 @@
 import scrapy
 
-
 class MoulaviSpider(scrapy.Spider):
 	name = "moulavi"
-
-	def start_requests(self):
-		start_urls = ['https://ganjoor.net/moulavi/masnavi/daftar1/sh1/','https://ganjoor.net/moulavi/masnavi/daftar1/sh2/']
-		for url in start_urls:
-			yield scrapy.Request(url=url, callback=self.parse)
+	start_urls = [
+		'https://ganjoor.net/moulavi/masnavi/daftar1/sh1/',
+		'https://ganjoor.net/moulavi/masnavi/daftar1/sh2/',
+	]
 
 	def parse(self, response):
 		for line in response.css('div.b'):
 			yield {
 				'right_column': line.css('div.m1').extract(),
 				'left_column': line.css('div.m2').extract(),
-			}
+}
