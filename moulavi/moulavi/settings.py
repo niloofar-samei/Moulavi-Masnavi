abf2f5c6c2b8efb24426de915f7a20259e63e3da
niloofar <niloofar.samei@gmail.com>
2018-05-18 19:07:01 +0430

    add FEED_EXPORT_ENCODING=utf-8 to settings.py again

--- moulavi/moulavi/settings.py
+++ moulavi/moulavi/settings.py
@@ -13,7 +13,7 @@ BOT_NAME = 'moulavi'
 
 SPIDER_MODULES = ['moulavi.spiders']
 NEWSPIDER_MODULE = 'moulavi.spiders'
-
+FEED_EXPORT_ENCODING = 'utf-8' 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'moulavi (+http://www.yourdomain.com)'
