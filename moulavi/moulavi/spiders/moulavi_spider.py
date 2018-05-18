6774db1d903595ac24b2a100a3fb9a503c5604c8
niloofar <niloofar.samei@gmail.com>
2018-05-18 21:57:18 +0430

    getting title and the first and the second hemistichs of the first and the second page

--- moulavi/moulavi/spiders/moulavi_spider.py
+++ moulavi/moulavi/spiders/moulavi_spider.py
@@ -1,15 +1,21 @@
 import scrapy
 
+
 class MoulaviSpider(scrapy.Spider):
 	name = "moulavi"
-	start_urls = [
-		'https://ganjoor.net/moulavi/masnavi/daftar1/sh1/',
-		'https://ganjoor.net/moulavi/masnavi/daftar1/sh2/',
-	]
+
+	def start_requests(self):
+		start_urls = [
+			'https://ganjoor.net/moulavi/masnavi/daftar1/sh1/',
+			'https://ganjoor.net/moulavi/masnavi/daftar1/sh2/',
+		]
+		for url in start_urls:
+			yield scrapy.Request(url=url, callback=self.parse)
 
 	def parse(self, response):
 		for line in response.css('div.b'):
 			yield {
+				'title': response.css('h2 a::text').extract(),
 				'right_column': line.css('div.m1').extract(),
 				'left_column': line.css('div.m2').extract(),
-}
+			}
